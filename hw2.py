d8d14a5cbc7a023cd2ef50aaf36caede5e6dc8e8
santi <sgiron13@verizon.net>
2018-11-05 02:22:14 -0600

    complete find_interest_points

---
+++ hw2.py
@@ -0,0 +1,270 @@
+import numpy as np
+from canny import *
+
+"""
+   INTEREST POINT OPERATOR (12 Points Implementation + 3 Points Write-up)
+
+   Implement an interest point operator of your choice.
+
+   Your operator could be:
+
+   (A) The Harris corner detector (Szeliski 4.1.1)
+
+               OR
+
+   (B) The Difference-of-Gaussians (DoG) operator defined in:
+       Lowe, "Distinctive Image Features from Scale-Invariant Keypoints", 2004.
+       https://www.cs.ubc.ca/~lowe/papers/ijcv04.pdf
+
+               OR
+
+   (C) Any of the alternative interest point operators appearing in
+       publications referenced in Szeliski or in lecture
+
+              OR
+
+   (D) A custom operator of your own design
+
+   You implementation should return locations of the interest points in the
+   form of (x,y) pixel coordinates, as well as a real-valued score for each
+   interest point.  Greater scores indicate a stronger detector response.
+
+   In addition, be sure to apply some form of spatial non-maximum suppression
+   prior to returning interest points.
+
+   Whichever of these options you choose, there is flexibility in the exact
+   implementation, notably in regard to:
+
+   (1) Scale
+
+       At what scale (e.g. over what size of local patch) do you operate?
+
+       You may optionally vary this according to an input scale argument.
+
+       We will test your implementation at the default scale = 1.0, so you
+       should make a reasonable choice for how to translate scale value 1.0
+       into a size measured in pixels.
+
+   (2) Nonmaximum suppression
+
+       What strategy do you use for nonmaximum suppression?
+
+       A simple (and sufficient) choice is to apply nonmaximum suppression
+       over a local region.  In this case, over how large of a local region do
+       you suppress?  How does that tie into the scale of your operator?
+
+   For making these, and any other design choices, keep in mind a target of
+   obtaining a few hundred interest points on the examples included with
+   this assignment, with enough repeatability to have a large number of
+   reliable matches between different views.
+
+   If you detect more interest points than the requested maximum (given by
+   the max_points argument), return only the max_points highest scoring ones.
+
+   In addition to your implementation, include a brief write-up (in hw2.pdf)
+   of your design choices.
+
+   Arguments:
+      image       - a grayscale image in the form of a 2D numpy array
+      max_points  - maximum number of interest points to return
+      scale       - (optional, for your use only) scale factor at which to
+                    detect interest points
+
+   Returns:
+      xs          - numpy array of shape (N,) containing x-coordinates of the
+                    N detected interest points (N <= max_points)
+      ys          - numpy array of shape (N,) containing y-coordinates
+      scores      - numpy array of shape (N,) containing a real-valued
+                    measurement of the relative strength of each interest point
+                    (e.g. corner detector criterion OR DoG operator magnitude)
+"""
+def find_interest_points(image, max_points = 200, scale = 1.0):
+   # check that image is grayscale
+   assert image.ndim == 2, 'image should be grayscale'
+   ##########################################################################
+   # TODO: YOUR CODE HERE
+   # get the shape of the input
+   rows, cols = image.shape
+   # compute x and y gradients of the image
+   ix, iy = sobel_gradients(image);
+   # compute the outer products of gradients
+   i2x = ix * ix
+   ixiy = ix * iy
+   i2y = iy * iy
+   # convolve the outer products with a larger gaussian
+   a_i2x = conv_2d_gaussian(i2x, 2);
+   a_ixiy = conv_2d_gaussian(ixiy, 2);
+   a_i2y = conv_2d_gaussian(i2y, 2);
+   # create an array for interest measures
+   measures = np.zeros([rows, cols])
+   # calculate interest measures at each point
+   for row in range(rows):
+      for col in range(cols):
+         eigvals = LA.eigvals(([a_i2x[row, col], a_ixiy[row, col]],
+                               [a_ixiy[row, col], a_i2y[row, col]]))
+         # input harmonic mean interest measurement
+         w1, w2 = eigvals
+         measures[row, col] = w1*w2/(w1+w2)
+   pad_measures = mirror_border(measures)
+   points = []
+   for row in range(rows):
+      for col in range(cols):
+         local_max = np.max(pad_measures[row:row+3, col:col+3])
+         # check if measurement is the local max
+         if measures[row, col] == local_max:
+            points.append((row, col, measures[row, col]))
+   # sort list of interest points by measurement
+   points.sort(key=operator.itemgetter(2), reverse=True)
+   # get highest 200 interest points
+   points = points[:200]
+   # return coordinates and scores for interest points
+   xs = [point[0] for point in points]
+   ys = [point[1] for point in points]
+   scores = [point[2] for point in points]
+   ##########################################################################
+   return xs, ys, scores
+
+"""
+   FEATURE DESCRIPTOR (12 Points Implementation + 3 Points Write-up)
+
+   Implement a SIFT-like feature descriptor by binning orientation energy
+   in spatial cells surrounding an interest point.
+
+   Unlike SIFT, you do not need to build-in rotation or scale invariance.
+
+   A reasonable default design is to consider a 3 x 3 spatial grid consisting
+   of cell of a set width (see below) surrounding an interest point, marked
+   by () in the diagram below.  Using 8 orientation bins, spaced evenly in
+   [-pi,pi), yields a feature vector with 3 * 3 * 8 = 72 dimensions.
+
+             ____ ____ ____
+            |    |    |    |
+            |    |    |    |
+            |____|____|____|
+            |    |    |    |
+            |    | () |    |
+            |____|____|____|
+            |    |    |    |
+            |    |    |    |
+            |____|____|____|
+
+                 |----|
+                  width
+
+   You will need to decide on a default spatial width.  Optionally, this can
+   be a multiple of a scale factor, passed as an argument.  We will only test
+   your code by calling it with scale = 1.0.
+
+   In addition to your implementation, include a brief write-up (in hw2.pdf)
+   of your design choices.
+
+  Arguments:
+      image    - a grayscale image in the form of a 2D numpy
+      xs       - numpy array of shape (N,) containing x-coordinates
+      ys       - numpy array of shape (N,) containing y-coordinates
+      scale    - scale factor
+
+   Returns:
+      feats    - a numpy array of shape (N,K), containing K-dimensional
+                 feature descriptors at each of the N input locations
+                 (using the default scheme suggested above, K = 72)
+"""
+def extract_features(image, xs, ys, scale = 1.0):
+   # check that image is grayscale
+   assert image.ndim == 2, 'image should be grayscale'
+   ##########################################################################
+   # TODO: YOUR CODE HERE
+   raise NotImplementedError('extract_features')
+   ##########################################################################
+   return feats
+
+"""
+   FEATURE MATCHING (7 Points Implementation + 3 Points Write-up)
+
+   Given two sets of feature descriptors, extracted from two different images,
+   compute the best matching feature in the second set for each feature in the
+   first set.
+
+   Matching need not be (and generally will not be) one-to-one or symmetric.
+   Calling this function with the order of the feature sets swapped may
+   result in different returned correspondences.
+
+   For each match, also return a real-valued score indicating the quality of
+   the match.  This score could be based on a distance ratio test, in order
+   to quantify distinctiveness of the closest match in relation to the second
+   closest match.  It could optionally also incorporate scores of the interest
+   points at which the matched features were extracted.  You are free to
+   design your own criterion.
+
+   In addition to your implementation, include a brief write-up (in hw2.pdf)
+   of your design choices.
+
+   Arguments:
+      feats0   - a numpy array of shape (N0, K), containing N0 K-dimensional
+                 feature descriptors (generated via extract_features())
+      feats1   - a numpy array of shape (N1, K), containing N1 K-dimensional
+                 feature descriptors (generated via extract_features())
+      scores0  - a numpy array of shape (N0,) containing the scores for the
+                 interest point locations at which feats0 was extracted
+                 (generated via find_interest_point())
+      scores1  - a numpy array of shape (N1,) containing the scores for the
+                 interest point locations at which feats1 was extracted
+                 (generated via find_interest_point())
+
+   Returns:
+      matches  - a numpy array of shape (N0,) containing, for each feature
+                 in feats0, the index of the best matching feature in feats1
+      scores   - a numpy array of shape (N0,) containing a real-valued score
+                 for each match
+"""
+def match_features(feats0, feats1, scores0, scores1):
+   ##########################################################################
+   # TODO: YOUR CODE HERE
+   raise NotImplementedError('match_features')
+   ##########################################################################
+   return matches, scores
+
+"""
+   HOUGH TRANSFORM (7 Points Implementation + 3 Points Write-up)
+
+   Assuming two images of the same scene are related primarily by
+   translational motion, use a predicted feature correspondence to
+   estimate the overall translation vector t = [tx ty].
+
+   Your implementation should use a Hough transform that tallies votes for
+   translation parameters.  Each pair of matched features votes with some
+   weight dependant on the confidence of the match; you may want to use your
+   estimated scores to determine the weight.
+
+   In order to accumulate votes, you will need to decide how to discretize the
+   translation parameter space into bins.
+
+   In addition to your implementation, include a brief write-up (in hw2.pdf)
+   of your design choices.
+
+   Arguments:
+      xs0     - numpy array of shape (N0,) containing x-coordinates of the
+                interest points for features in the first image
+      ys0     - numpy array of shape (N0,) containing y-coordinates of the
+                interest points for features in the first image
+      xs1     - numpy array of shape (N1,) containing x-coordinates of the
+                interest points for features in the second image
+      ys1     - numpy array of shape (N1,) containing y-coordinates of the
+                interest points for features in the second image
+      matches - a numpy array of shape (N0,) containing, for each feature in
+                the first image, the index of the best match in the second
+      scores  - a numpy array of shape (N0,) containing a real-valued score
+                for each pair of matched features
+
+   Returns:
+      tx      - predicted translation in x-direction between images
+      ty      - predicted translation in y-direction between images
+      votes   - a matrix storing vote tallies; this output is provided for
+                your own convenience and you are free to design its format
+"""
+def hough_votes(xs0, ys0, xs1, ys1, matches, scores):
+   ##########################################################################
+   # TODO: YOUR CODE HERE
+   raise NotImplementedError('hough_votes')
+   ##########################################################################
+   return tx, ty, votes
